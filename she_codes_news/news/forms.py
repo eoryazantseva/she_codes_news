2a480e62aa45d43496bec36da4ae77153e860404
eoryazantseva <eoryazantseva@gmail.com>
2023-07-17 16:16:50 +1000

    styled the form for adding new stories

--- she_codes_news/news/forms.py
+++ she_codes_news/news/forms.py
@@ -7,20 +7,33 @@ class StoryForm(ModelForm):
     class Meta:
         model = NewsStory
         fields = ['title','pub_date', 'content', 'image_url']
+        labels = {
+            'title': 'Your Article Title',
+            'pub_date': 'Publication date',
+            'content': 'The Text Of The Article',
+            'image_url': 'Image URL'
+        }
         widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
             'pub_date': forms.DateInput(
                 format='%d/%m/%Y',
                 attrs={
                     'class': 'form-control',
                     'placeholder': 'Select a date',
                     'type': 'date'
-                }
-            ),
+                    }
+                ),
+            'content': forms.Textarea(
+                attrs={
+                    'class': 'form-control',
+                    'rows': 5
+                    }
+                ),
             'image_url': forms.URLInput(
                 attrs={
                     'class': 'form-control',
                     'placeholder': 'Enter the image URL'
-                }
-            )
+                    }
+                )
         }
     
